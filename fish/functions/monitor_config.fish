081b99ae9e4a2fb6673d80fc1c98ed4b9beb9a94
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2019-06-30 11:36:56 -0700

    Change alacritty font when dockeda

--- fish/functions/monitor_config.fish
+++ fish/functions/monitor_config.fish
@@ -8,13 +8,15 @@ function monitor_config -a mode -d "Call xrandr to set monitor mode. Mode: {mobi
 		xrandr --output eDP-1 --auto
 		echo "Xft.dpi: 180" > ~/.Xresources
 		cp ~/.config/polybar/.config.mobile ~/.config/polybar/config
+		cp ~/.config/alacritty/.alacritty.yml.mobile ~/.config/alacritty/alacritty.yml;
 		sh ~/.fehbg;
 		echo "Configuration completed. Please restart lighdm to fix DPI"
 	    case docked
 		echo "Initializing docked mode..."
-		xrandr --output DP-2 --mode 2560x1440 --output eDP-1 --off
+		xrandr --output DP-2 --auto --output eDP-1 --off
 		echo "! Xft.dpi: 180" > ~/.Xresources
 		cp ~/.config/polybar/.config.docked ~/.config/polybar/config
+		cp ~/.config/alacritty/.alacritty.yml.docked ~/.config/alacritty/alacritty.yml;
 		sh ~/.fehbg;
 		echo "Configuration completed. Please restart lighdm to fix DPI"
 	    case '*'
