afad3943d7d6b80dd0d32e3e8f0497ad1540a96f
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2021-02-18 14:49:50 -0800

    first try at updating system

---
+++ update_system.fish
@@ -0,0 +1,27 @@
+#! /usr/bin/env fish
+
+# First, copy all the files
+# WM stuff
+cp i3/config ~/.config/i3/config
+cp -r ./polybar ~/.config/polybar
+cp ./dunstrc ~/.config/dunst/dunstrc
+
+# Terminal
+cp ./alacritty.yml ~/.config/alacritty/alacritty.yml
+cp -r ./fish ~/.config/fish
+
+# Editor stuff
+cp ./.editorconfig ~/.editorconfig
+cp -r ./.doom.d ~/.doom.d
+
+# LeftWM
+cp -r ./leftwm ~/.config/leftwm
+cp -r ./picom ~/.config/picom
+
+# Cargo!
+cp ./.cargo/* ~/.cargo/
+
+# Git!
+cp ./.gitconfig ~/.gitconfig
+
+echo "Done!"
